60a2939cf8f35281ad4ed8f8decea77040927384
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2023-03-15 12:38:43 +0530

    delete from firebase

--- lib/ui/firebase_database/post_screen.dart
+++ lib/ui/firebase_database/post_screen.dart
@@ -83,6 +83,10 @@ class _PostScreenState extends State<PostScreen> {
                       PopupMenuItem(
                           value:1,
                           child: ListTile(
+                            onTap: (){
+                              Navigator.pop(context);
+                             ref.child(snapshot.child('id').value.toString()).remove();
+                            },
                             leading: Icon(Icons.delete),
                             title: Text('Delete'),
                           )
